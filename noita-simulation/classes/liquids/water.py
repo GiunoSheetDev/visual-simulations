a9cdd493c71aa1c0be522208723d1961d99ce2b3
Lorenzo Benatti <96290350+GiunoSheetDev@users.noreply.github.com>
2024-05-01 00:32:38 +0200

    Delete noita-simulation directory

--- noita-simulation/classes/liquids/water.py
+++
@@ -1,9 +0,0 @@
-from classes.liquids.liquid import Liquid
-
-class Water(Liquid):
-    def __init__(self, i, j) -> None:
-        super().__init__(i, j)
-        self.color = (43, 156, 237)
-
-    def update(self, tiles: list[list]) -> list[list]:
-        return super().update(tiles)
--- noita-simulation/classes/solids/dynamic/__init__.py
+++
@@ -1,1 +0,0 @@
-__all__ = ["dynamicSolid", "sand"]
